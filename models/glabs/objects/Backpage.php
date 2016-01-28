738395c15a81a4a01b2f9968b7c6d0e472320a37
Nikolaj Rudakov <nnrudakov@gmail.com>
2016-01-28 22:10:26 +0300

    Changed get images.

--- models/glabs/objects/Backpage.php
+++ models/glabs/objects/Backpage.php
@@ -75,7 +75,7 @@ class Backpage extends BaseObject
             if (false === strpos($url, '.jpg')) {
                 $url = $imageTag->getAttribute('src');
                 if (!$layout &&
-                    (false === strpos($url, 'GetImage.aspx') || false === strpos($url, 'images.psndealer.com'))) {
+                    false === strpos($url, 'GetImage.aspx') && false === strpos($url, 'images.psndealer.com')) {
                     continue;
                 }
             }
