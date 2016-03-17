ed6a56eccbc8084d2805ba6eb8389c1cf52acf26
Nikolaj Rudakov <n.rudakov@itsirius.ru>
2016-03-17 17:08:48 +0300

    Some improvements.

--- models/glabs/categories/chatapp/Backpage.php
+++ models/glabs/categories/chatapp/Backpage.php
@@ -54,7 +54,7 @@ class Backpage extends BaseCategory
         if (false !== strpos($dom, 'No matches found')  || false !== strpos($dom, 'Keine Entsprechungen gefunden')  ||
             false !== strpos($dom, 'No hay resultados') || false !== strpos($dom, 'Nessuna corrispondenza trovata') ||
             false !== strpos($dom, 'Aucune correspondance n&#146;a &eacute;t&eacute; trouv&eacute;e') ||
-            false !== strpos($dom, 'Nenhuma correspondência encontrada')) {
+            false !== strpos($dom, 'Nenhuma correspondência encontrada') || false !== strpos($dom, 'Совпадений нет')) {
             return true;
         }
 
