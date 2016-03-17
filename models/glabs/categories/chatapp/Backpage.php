38c5679cfe83b0687dc959339b8d6b6b7f10d1a7
Nikolaj Rudakov <n.rudakov@itsirius.ru>
2016-03-17 12:36:43 +0300

    Some improvements.

--- models/glabs/categories/chatapp/Backpage.php
+++ models/glabs/categories/chatapp/Backpage.php
@@ -24,7 +24,7 @@ class Backpage extends BaseCategory
      */
     public function __construct($url, $title, $categoryId, $type, $count)
     {
-        parent::$pageParam = '&page=';
+        parent::$pageParam = '?page=';
         parent::__construct($url, $title, $categoryId, $type, $count);
     }
 
